c630657eab4fa15520a6aaa8b5c04a2e31906fe1
Alex Clark <clarkie4114@gmail.com>
2022-03-14 19:46:57 -0400

    Recentering icons

--- components/Home.vue
+++ components/Home.vue
@@ -19,13 +19,13 @@
         resume
       </a>
       <div class="flex mt-4">
-        <a href="https://www.linkedin.com/in/raclark-5a5a6971" target="_blank" class="flex bg-white text-sky-600 active:bg-sky-700 active:text-white dark:bg-zinc-600 dark:text-sky-300 dark:active:bg-zinc-400 font-bold uppercase text-2xl px-3 py-2 rounded-xl shadow hover:shadow-lg outline-none focus:outline-noe mr-3 mb-1 w-12 h-12 ease-linear transition-all duration-150">
+        <a href="https://www.linkedin.com/in/raclark-5a5a6971" target="_blank" class="flex items-center bg-white text-sky-600 active:bg-sky-700 active:text-white dark:bg-zinc-600 dark:text-sky-300 dark:active:bg-zinc-400 font-bold uppercase text-2xl px-3 py-2 rounded-xl shadow hover:shadow-lg outline-none focus:outline-noe mr-3 mb-1 w-12 h-12 ease-linear transition-all duration-150">
           <font-awesome-icon class="" icon="fa-brands fa-linkedin" />
         </a>
-        <a href="https://github.com/bigal41" target="_blank" class="flex bg-white text-sky-600 active:bg-sky-700 active:text-white dark:bg-zinc-600 dark:text-sky-300 dark:active:bg-zinc-400 font-bold uppercase text-2xl px-3 py-2 rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mr-3 mb-1 w-12 h-12 ease-linear transition-all duration-150">
+        <a href="https://github.com/bigal41" target="_blank" class="flex items-center bg-white text-sky-600 active:bg-sky-700 active:text-white dark:bg-zinc-600 dark:text-sky-300 dark:active:bg-zinc-400 font-bold uppercase text-2xl px-3 py-2 rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mr-3 mb-1 w-12 h-12 ease-linear transition-all duration-150">
           <font-awesome-icon icon="fa-brands fa-github" />
         </a>
-        <a href="mailto:ralexclark@outlook.com" class="flex bg-white text-sky-600 active:bg-sky-700 active:text-white dark:bg-zinc-600 dark:text-sky-300 dark:active:bg-zinc-400 font-bold uppercase text-2xl px-3 py-2 rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mb-1 w-12 h-12 ease-linear transition-all duration-150">
+        <a href="mailto:ralexclark@outlook.com" class="flex items-center bg-white text-sky-600 active:bg-sky-700 active:text-white dark:bg-zinc-600 dark:text-sky-300 dark:active:bg-zinc-400 font-bold uppercase text-2xl px-3 py-2 rounded-xl shadow hover:shadow-lg outline-none focus:outline-none mb-1 w-12 h-12 ease-linear transition-all duration-150">
           <font-awesome-icon icon="fa-solid fa-envelope" />
         </a>
       </div>
